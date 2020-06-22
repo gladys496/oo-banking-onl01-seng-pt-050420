4ff2b4377a21bbc2f644293cea17f86b8c3359c5
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-06-22 08:22:28 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,8 +18,8 @@ class Transfer
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
-    else
-      false && @status = "rejected"
+    else false 
+       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
     end
   end
