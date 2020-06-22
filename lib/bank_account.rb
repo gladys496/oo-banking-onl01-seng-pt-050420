0298687ae7c261ec8a976d635e2bebe41b8ee8f4
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-06-22 08:17:59 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,34 +1,41 @@
 class BankAccount
-  
-  attr_accessor :balance, :status 
-  
-  attr_reader :name 
-  
-  def initialize(name)
-    @name = name 
-    @balance = 1000
+attr_reader :name
+attr_accessor :balance, :status
+
+ @@all = []
+
+ def initialize(name,balance=1000)
+    @name = name
+    @balance = balance
     @status = "open"
-   end
-   
-     def deposit(amount)
-    @balance += amount
-  end
-
-  def display_balance 
-    "Your balance is $#{balance}."
-  end 
- 
- def valid?
-    if @status == "open" && @balance > 0
-      true
-    else
-      false
-    end
-   
- end 
- 
- def close_account
-    @status = "closed"
-  end
-
+    @@all << self
 end
+
+ def deposit (amount)
+   @balance += amount
+end
+
+ def display_balance 
+     "Your balance is $#{self.balance}."
+end
+
+ def valid?
+    if @balance <= 0 
+        false
+    elsif @status == "closed"
+        false
+    else
+        true
+    end
+end
+
+ def close_account
+  self.status = "closed"
+end
+
+ def self.all
+    @@all
+end
+
+
+ end	end
