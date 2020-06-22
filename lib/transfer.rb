26298ea54e37490454fdead8d94cc0d1862061c0
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-06-22 08:11:27 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,48 @@
+
 class Transfer
-  # your code here
+  # code here
+  attr_accessor :sender, :receiver, :amount, :status, :count
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def valid?
+    if sender.valid? && receiver.valid?
+      true
+    else
+      false
+    end
+  end
+  
+  def execute_transaction
+    if self.valid? == true && self.status == "pending"
+        @sender.balance -= @amount
+        @receiver.balance += @amount
+        self.status = "complete"
+    else self.valid? == false && self.status == "rejected" 
+     
+      "Transaction rejected. Please check your account balance."
+      
+    
+    end
+end 
+  
+
+  def reverse_transfer
+    if @status == "complete"
+      @sender.deposit( @amount ) 
+      @receiver.deposit( @amount * -1)
+      @status = "reversed"
+    end
+  end
+
 end
+
+    
+    
+  
+
